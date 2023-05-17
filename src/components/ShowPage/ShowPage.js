c28505ab486882af97f9592489f826457549af91
outjam <123077972+Culture3k-lab@users.noreply.github.com>
2023-05-17 15:33:59 +0300

    add

--- src/components/ShowPage/ShowPage.js
+++ src/components/ShowPage/ShowPage.js
@@ -14,7 +14,7 @@ function ShowPage() {
                 <p className='title'>Оставить заявку для бизнеса</p>
                 <p className='description'>Leave a request for business</p>
                 </div>
-                <Button name='Узнать подробнее' type='tertiary' link='mailto:sales@km20water.ru' />
+                <Button name='Узнать подробнее' type='tertiary' link='mailto:pr@km20water.ru' />
             </div>
             <p className='showPage_information_Content'>Бутылка, выполненная из&nbsp;стекла и&nbsp;подчёркивает чистый вкус воды. Дизайн этикетки минималистичный, но&nbsp;для партнеров из&nbsp;ресторанов и&nbsp;отелей будет доступна опция кастомизировать её&nbsp;в&nbsp;соответствии с&nbsp;собственным фирменным стилем</p>
         </div>
